a91944cd3751e90e09bd71864d78acf029341660
Alen <hernandezgiovannic13@gmail.com>
2023-11-02 14:22:26 -0600

    Conexion

--- connection/conexion.php
+++ connection/conexion.php
@@ -1,12 +1,12 @@
 
 <?php
-$link= "mysql:host=localhost;dbname=prueba_tecnica";
+$link= "mysql:host=127.0.0.1;dbname=prueba_tecnica";
 $username = "root";
-$password = "";
+$password = "zetsu611212";
 
 try {
   $pdo = new PDO($link, $username, $password);
-
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
   echo "Conexión  Fallida: " . $e->getMessage();
 }
